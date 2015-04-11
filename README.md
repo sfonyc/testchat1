a9cbb26b091ea3585390feb49516e3cd4a62990e
Jayanth Rasamsetti <jaysetty@gmail.com>
2015-04-11 18:52:22 -0400

    add ionic files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# testchat1
+# ionic-firebase
+Real Time multi person Chat App using Ionic &amp; Firebase
+
+It uses the AngularFire library to connect to firebase.
---
+++ www/js/app.js
@@ -0,0 +1,97 @@
+var firebaseUrl = "https://xyz.firebaseapp.com";
+
+function onDeviceReady() {
+    angular.bootstrap(document, ["mychat"]);
+}
+document.addEventListener("deviceready", onDeviceReady, false);
+
+angular.module('mychat', ['ionic', 'firebase', 'angularMoment', 'mychat.controllers', 'mychat.services'])
+
+.run(function ($ionicPlatform, $rootScope, $location, Auth, $ionicLoading) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+        ionic.Platform.fullScreen();
+
+        $rootScope.firebaseUrl = firebaseUrl;
+        $rootScope.displayName = null;
+
+        Auth.$onAuth(function (authData) {
+            if (authData) {
+                console.log("Logged in as:", authData.uid);
+            } else {
+                console.log("Logged out");
+                $ionicLoading.hide();
+                $location.path('/login');
+            }
+        });
+
+        $rootScope.logout = function () {
+            console.log("Logging out from the app");
+            $ionicLoading.show({
+                template: 'Logging Out...'
+            });
+            Auth.$unauth();
+        }
+
+
+        $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+            if (error === "AUTH_REQUIRED") {
+                $location.path("/login");
+            }
+        });
+    });
+})
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    console.log("setting config");
+    $stateProvider
+    .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl',
+        resolve: {
+            "currentAuth": ["Auth",
+                function (Auth) {
+                    return Auth.$waitForAuth();
+        }]
+        }
+    })
+
+    .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html",
+        resolve: {
+            "currentAuth": ["Auth",
+                function (Auth) {
+                    return Auth.$requireAuth();
+      }]
+        }
+    })
+    .state('tab.rooms', {
+        url: '/rooms',
+        views: {
+            'tab-rooms': {
+                templateUrl: 'templates/tab-rooms.html',
+                controller: 'RoomsCtrl'
+            }
+        }
+    })
+
+    .state('tab.chat', {
+        url: '/chat/:roomId',
+        views: {
+            'tab-chat': {
+                templateUrl: 'templates/tab-chat.html',
+                controller: 'ChatCtrl'
+            }
+        }
+    })
+    $urlRouterProvider.otherwise('/login');
+
+});
